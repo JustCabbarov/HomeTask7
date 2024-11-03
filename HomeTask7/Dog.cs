097070b87a8d0a93a51011f92b4a161e65775d53
Əli Cabbarov <elicabbarov88@gmail.com>
2024-11-03 01:00:52 -0800

    HomeTask7 Created

---
+++ HomeTask7/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    public class Dog : Animal
+    {
+        public int Age;
+        public Dog()
+        {
+
+        }
+        public Dog(string name):base(name) 
+        {
+            
+        }
+        public Dog(string name, double weight) : base(name)
+        {
+            
+        }
+        public Dog(string name, double weight, string type) : base(name, weight)
+        {
+            
+        }
+        public Dog(string name, double weight, string type, string species) : base(name, weight, type)
+        {
+            
+        }
+        public Dog(string name, double weight, string type, string species,int age) :this ( name,  weight,  type,  species)
+        {
+            Age = age;
+        }
+        public void FullDataOfDog()
+        {
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}, Type: {Type}, Species: {Species}, Age: {Age}");
+        }
+
+    }
+}
---
+++ HomeTask7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace ClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal anml1 = new Animal("Mestan",7.4,"Pisik","Schotish");
+            anml1.FullDataOfAnimal();
+            Dog dog1 = new Dog("Leo",15, "Dog" , "Labrador" ,4);
+            dog1.FullDataOfDog();
+        }
+    }
+    public class Animal
+    {
+       public string Name;
+        public double Weight;
+        public string Type;
+        public string Species;
+
+        public Animal()
+        {
+            
+        }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public Animal(string name,double weight) :this(name)
+        {
+            Weight = weight;
+        }
+        public Animal(string name,double weight,string type): this( name,  weight)
+        {
+            Type = type;
+        }
+        public Animal(string name, double weight, string type, string species) : this( name, weight,  type) 
+        {
+            Species = species;
+        }
+        public void FullDataOfAnimal()
+        {
+            Console.WriteLine($"Name: {Name}, Weight: {Weight}, Type: {Type}, Species: {Species}");
+        }
+
+    }
+
+
+}
